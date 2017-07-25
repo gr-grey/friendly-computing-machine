4b979279282eec7684c8e9394d776f6414fd2981
Grey <grui@sas.upenn.edu>
2017-07-25 14:31:53 -0400

    starting a computing file

---
+++ friendly_computing_machine/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the base file of the friendly-coputing-machine
+"""
+
+from . import math
+from .math import mult
---
+++ friendly_computing_machine/math.py
@@ -0,0 +1,19 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+#Write a function named add that adds two values
+
+def add(a, b):
+    """
+    Function that adds two values.
+    """
+    return a+b
+
+
+def mult(a, b):
+    """
+    Fuction that multiplies 2 values
+    """
+    return a*b
+	
